502a2f42009eff7a4904690f35bd6b64e6c72793
deadprogrammer <ron.evans@gmail.com>
2009-04-13 17:28:31 -0700

    Commit 1.2.1 version from RubyForge to migrate over here to github

---
+++ lib/hebruby.rb
@@ -0,0 +1,251 @@
+# Hebruby is a Ruby library to convert julian dates to hebrew dates, and vice-versa.
+# Written by Ron Evans (ron dot evans at gmail dot com or http://www.deadprogrammersociety.com).
+# Additional code contributed by Joshua Harvey.
+# Based on Javascript code from John Walker (http://www.fourmilab.ch/documents/calendar/).
+# Usage:      
+#       For julian to hebrew:
+#             @hb = Hebruby::HebrewDate.new(Date.new(2010, 1, 1))
+#             assert_equal(10, @hb.month, "Wrong month.")
+#             assert_equal("Tevet", @hb.month_name, "Wrong month name.")
+#             assert_equal(5770, @hb.year, "Wrong year.")
+#             assert_equal(15, @hb.day, "Wrong day.")
+#
+#       For hebrew to julian:
+#             @hb = Hebruby::HebrewDate.new1(15,10,5770)
+#             assert_equal(Date.new(2010, 1, 1).jd, @hb.jd, "Wrong Julian date.")
+
+require 'jcode'
+$KCODE = 'u' # Always use UTF-8 internally!
+require 'date'
+
+module Hebruby
+
+  class HebrewDate
+    HEBREW_EPOCH = 347995.5
+    MONTH_NAMES = %w{none Nissan Iyar Sivan Tamuz Av Elul Tishrei Chesvan Kislev Tevet Shvat Adar Veadar}
+    HEB_MONTH_NAMES = [ nil, 'ניסן', 'אייר', 'סיון', 'תמוז', 'אב', 'אלול', 'תשרי', 
+      'חשון', 'כסלו', 'טבת', 'שבט', 'אדר', 'אדר א\'', 'אדר ב\'']
+    HEB_DAYS = [ nil, 'א\'', 'ב\'', 'ג\'', 'ד\'', 'ה\'', 'ו\'', 'ז\'', 'ח\'', 'ט\'', 
+      'י\'', 'י"א', 'י"ב', 'י"ג', 'י"ד', 'ט"ו', 'ט"ז', 'י"ז', 'י"ח', 'י"ח', 
+      'י"ט', 'כ\'' , 'כ"א', 'כ"ב', 'כ"ג', 'כ"ד', 'כ"ה', 'כ"ו', 'כ"ז', 'כ"ט', 'ל\'' ]
+     ONES = [ '', 'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט' ]
+    TENS = [ '', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ' ] 
+
+
+    # Accessors for base Hebrew day, month, and year
+   attr_accessor :hd, :hm, :hy
+    
+    # standard constructor from julian to hebrew date
+    def initialize(jd=nil)
+      if jd
+        @jd = jd
+        convert_from_julian
+      end
+    end
+    
+    # additional constructor from hebrew date to julian
+    def self.new1(hd, hm, hy)
+      me = new
+      me.hd = hd
+      me.hm = hm
+      me.hy = hy
+      me.convert_from_hebrew
+      return me
+    end
+    
+    def day
+      return @hd
+    end
+
+  # Provide correct Hebrew transliterated month display name
+    def month
+      return @hm
+    end
+
+    # return Hebrew year converted from julian date
+    def year
+      return @hy
+    end
+
+    # return julian date converted from hebrew date
+    def jd
+      return @jd + 1
+    end
+    
+   # Provide correct Hebrew transliterated month display name
+   def month_name
+      return month_display_name(@hm)
+    end
+
+   # Provide correct Hebrew month display name
+    def heb_month_name
+      return heb_month_display_name(@hm)
+    end
+
+   # Provide correct Hebrew day display name
+    def heb_day_name
+      return HEB_DAYS[@hd]
+    end
+
+   # Provide correct Hebrew year display name
+    def heb_year_name
+      year = @hy
+      raise RangeError, "only 5700 - 5899 supported" if year < 5700 || year >= 5900
+      prefix = year / 100 == 57 ? "התש" : "התת"
+      suffix = heb_number(year % 100)
+      full = prefix + suffix
+    end
+
+   # Provide correct complete Hebrew display date
+    def heb_date
+      return heb_day_name + " ב" + heb_month_name + " " + heb_year_name
+    end
+
+   # Provide correct Hebrew number display
+    def heb_number(num)
+      return 'ט"ו' if num == 15
+      return 'ט"ז' if num == 16
+      if num < 10
+        return '"' + ONES[ num % 10 ] 
+      elsif num % 10 == 0
+        return '"' + TENS[ num / 10 ]
+      else
+        return TENS[ num / 10 ] + '"' + ONES[ num % 10 ]
+      end
+    end
+
+    # Provide correct Hebrew transliterated month display name
+    def month_display_name(month)
+      return MONTH_NAMES[month]  
+    end
+    
+   # Provide correct Hebrew month display name
+    def heb_month_display_name(month)
+      return HEB_MONTH_NAMES[month]  
+    end
+
+    def convert_from_julian
+      dateArray = jd_to_hebrew(@jd)
+      @hy = dateArray[0]		
+      @hm = dateArray[1]		
+      @hd = dateArray[2]		
+    end
+    
+    def convert_from_hebrew
+      @jd = to_jd(@hy, @hm, @hd).jd
+    end
+    
+    # Is a given Hebrew year a leap year ?
+    def leap?(year)
+      return ((year * 7) + 1).modulo(19) < 7
+    end
+
+    # How many months are there in a Hebrew year (12 = normal, 13 = leap)
+    def year_months(year)
+      return leap?(year) ? 13 : 12
+    end
+
+    # How many days are in a Hebrew year ?
+    def year_days(year)
+      return to_jd(year + 1, 7, 1) - to_jd(year, 7, 1)
+    end
+
+    # How many days are in a given month of a given year
+    def month_days(year, month)
+      # First of all, dispose of fixed-length 29 day months
+      case
+        when (month == 2 || month == 4 || month == 6 || month == 10 || month == 13)
+          return 29
+
+        # If it's not a leap year, Adar has 29 days
+        when (month == 12 && !leap?(year)) then
+          return 29
+
+        # If it's Cheshvan, days depend on length of year
+        when month == 8 && year_days(year).modulo(10) != 5 then
+          return 29
+
+        # Similarly, Kislev varies with the length of year
+        when (month == 9 && (year_days(year).modulo(10) == 3)) then
+          return 29
+        
+        # Nope, it's a 30 day month
+        else
+          return 30
+      end
+    end
+
+private
+    # Test for delay of start of new year and to avoid
+    # Sunday, Wednesday, and Friday as start of the new year.
+    def delay_1(year)
+      months = (((235 * year) - 234) / 19).floor
+      parts = 12084 + (13753 * months)
+      day = (months * 29) + (parts / 25920).floor
+
+      if ((3 * (day + 1)).modulo(7) < 3)
+        day += 1
+      end
+      return day
+    end
+
+    # Check for delay in start of new year due to length of adjacent years
+    def delay_2(year)
+      last = delay_1(year - 1)
+      present = delay_1(year)
+      nextone = delay_1(year + 1)
+
+      return ((nextone - present) == 356) ? 2 : (((present - last) == 382) ? 1 : 0)
+    end
+
+    # Convert hebrew date to julian date
+    def to_jd(year, month, day)
+      months = year_months(year)
+      jd = HEBREW_EPOCH + delay_1(year) + delay_2(year) + day + 1
+
+      if (month < 7) then
+        for mon in 7..months
+          jd += month_days(year, mon)
+        end
+
+        for mon in 1..(month - 1)
+          jd += month_days(year, mon)
+        end
+      else
+        for mon in 7..(month - 1)
+          jd += month_days(year, mon)
+        end
+      end
+
+      return Object::Date.new1(jd)
+    end
+
+    # Convert Julian date to Hebrew date
+    # This works by making multiple calls to
+    # to_jd, and is this very slow
+    def jd_to_hebrew(jd)
+      myjd = jd.jd
+      count = (((myjd - HEBREW_EPOCH) * 98496.0) / 35975351.0).floor
+      year = count - 1
+        
+      i = count
+      while myjd >= to_jd(i, 7, 1).jd
+        year += 1
+        i += 1
+      end
+
+      first = (myjd < to_jd(year, 1, 1).jd) ? 7 : 1
+      i = month = first
+        
+      while myjd > to_jd(year, i, month_days(year, i)).jd
+        month += 1
+        i += 1
+      end
+        
+      day = (myjd - to_jd(year, month, 1).jd) # + 1
+      return [year, month, day]
+    end
+
+  end # class
+
+end # module
---
+++ test/hebruby_tests.rb
@@ -0,0 +1,153 @@
+# Unit tests for Hebruby module to convert julian dates to hebrew dates, and vice-versa
+# Written by Ron Evans
+# Additional code contributed by Joshua Harvey
+# Based on Javascript code from John Walker (http://www.fourmilab.ch/documents/calendar/)
+require 'rubygems'
+require 'test/unit'
+require 'hebruby'
+require 'date'
+
+class TC_MyTest2 < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  #
+  # hebrew to julian tests
+  #
+  
+  def test_h2j_1
+    @hb = Hebruby::HebrewDate.new(Date.new(2010, 1, 1))
+    assert_equal(10, @hb.month, "Wrong month.")
+    assert_equal("Tevet", @hb.month_name, "Wrong month name.")
+    assert_equal(5770, @hb.year, "Wrong year.")
+    assert_equal(15, @hb.day, "Wrong day.")
+  end
+
+  def test_h2j_2
+    @hb = Hebruby::HebrewDate.new(Date.new(2005, 1, 15))
+    assert_equal(11, @hb.month, "Wrong month.")
+    assert_equal("Shvat", @hb.month_name, "Wrong month name.")
+    assert_equal(5765, @hb.year, "Wrong year.")
+    assert_equal(5, @hb.day, "Wrong day.")
+  end
+
+  def test_h2j_3
+    @hb = Hebruby::HebrewDate.new(Date.new(2005, 4, 10))
+    assert_equal(1, @hb.month, "Wrong month.")
+    assert_equal("Nissan", @hb.month_name, "Wrong month name.")
+    assert_equal(5765, @hb.year, "Wrong year.")
+    assert_equal(1, @hb.day, "Wrong day.")
+  end
+
+  def test_h2j_4
+    @hb = Hebruby::HebrewDate.new(Date.new(1966, 4, 10))
+    assert_equal(1, @hb.month, "Wrong month.")
+    assert_equal("Nissan", @hb.month_name, "Wrong month name.")
+    assert_equal(5726, @hb.year, "Wrong year.")
+    assert_equal(20, @hb.day, "Wrong day.")
+  end
+
+  def test_h2j_5
+    @hb = Hebruby::HebrewDate.new(Date.new(1998, 12, 22))
+    assert_equal(10, @hb.month, "Wrong month.")
+    assert_equal("Tevet", @hb.month_name, "Wrong month name.")
+    assert_equal(5759, @hb.year, "Wrong year.")
+    assert_equal(3, @hb.day, "Wrong day.")
+  end
+
+  def test_h2j_6
+    @hb = Hebruby::HebrewDate.new(Date.new(1968, 6, 28))
+    assert_equal(4, @hb.month, "Wrong month.")
+    assert_equal("Tamuz", @hb.month_name, "Wrong month name.")
+    assert_equal(5728, @hb.year, "Wrong year.")
+    assert_equal(2, @hb.day, "Wrong day.")
+  end
+  
+  def test_h2j_7
+    @hb = Hebruby::HebrewDate.new(Date.new(1941, 12, 10))
+    assert_equal(9, @hb.month, "Wrong month.")
+    assert_equal("Kislev", @hb.month_name, "Wrong month name.")
+    assert_equal(5702, @hb.year, "Wrong year.")
+    assert_equal(20, @hb.day, "Wrong day.")
+  end
+
+  #
+  # julian to hebrew tests
+  #
+  
+  def test_j2h_1
+    @hb = Hebruby::HebrewDate.new1(15,10,5770)
+    assert_equal(15, @hb.day, "Wrong day.")
+    assert_equal(10, @hb.month, "Wrong month.")
+    assert_equal("Tevet", @hb.month_name, "Wrong month name.")
+    assert_equal(5770, @hb.year, "Wrong year.")
+    assert_equal('התש"ע', @hb.heb_year_name, "Wrong year.")
+    assert_equal(Date.new(2010, 1, 1).jd, @hb.jd, "Wrong Julian date.")
+  end
+
+  def test_j2h_2
+    @hb = Hebruby::HebrewDate.new1(5,11,5765)
+    assert_equal(5, @hb.day, "Wrong day.")
+    assert_equal(11, @hb.month, "Wrong month.")
+    assert_equal("Shvat", @hb.month_name, "Wrong month name.")
+    assert_equal(5765, @hb.year, "Wrong year.")
+    assert_equal(Date.new(2005, 1, 15).jd, @hb.jd, "Wrong Julian date.")
+  end
+
+  def test_j2h_3
+    @hb = Hebruby::HebrewDate.new1(1,1,5765)
+    assert_equal(1, @hb.day, "Wrong day.")
+    assert_equal(1, @hb.month, "Wrong month.")
+    assert_equal("Nissan", @hb.month_name, "Wrong month name.")
+    assert_equal(5765, @hb.year, "Wrong year.")
+    assert_equal(Date.new(2005, 4, 10).jd, @hb.jd, "Wrong Julian date.")
+  end
+
+  def test_j2h_4
+    @hb = Hebruby::HebrewDate.new1(20,1,5726)
+    assert_equal(20, @hb.day, "Wrong day.")
+    assert_equal(1, @hb.month, "Wrong month.")
+    assert_equal(5726, @hb.year, "Wrong year.")
+    assert_equal("Nissan", @hb.month_name, "Wrong month name.")
+    assert_equal(Date.new(1966, 4, 10).jd, @hb.jd, "Wrong Julian date.")
+  end
+
+  def test_j2h_5
+    @hb = Hebruby::HebrewDate.new1(3,10,5759)
+    assert_equal(3, @hb.day, "Wrong day.")
+    assert_equal(10, @hb.month, "Wrong month.")
+    assert_equal("Tevet", @hb.month_name, "Wrong month name.")
+    assert_equal(5759, @hb.year, "Wrong year.")
+    assert_equal(Date.new(1998, 12, 22).jd, @hb.jd, "Wrong Julian date.")
+    assert_equal(%q{ג' בטבת התשנ"ט}, @hb.heb_date)
+  end
+
+  def test_j2h_6
+    @hb = Hebruby::HebrewDate.new1(2,4,5728)
+    assert_equal(2, @hb.day, "Wrong day.")
+    assert_equal(4, @hb.month, "Wrong month.")
+    assert_equal("Tamuz", @hb.month_name, "Wrong month name.")
+    assert_equal("תמוז", @hb.heb_month_name, "Wrong month name.")    
+    assert_equal(5728, @hb.year, "Wrong year.")
+    assert_equal('התשכ"ח', @hb.heb_year_name, "Wrong year.")
+    assert_equal(Date.new(1968, 6, 28).jd, @hb.jd, "Wrong Julian date.")
+  end
+  
+  def test_j2h_7
+    @hb = Hebruby::HebrewDate.new1(20,9,5702)
+    assert_equal(20, @hb.day, "Wrong day.")
+    assert_equal(9, @hb.month, "Wrong month.")
+    assert_equal("Kislev", @hb.month_name, "Wrong month name.")
+    assert_equal("כסלו", @hb.heb_month_name, "Wrong month name.")    
+    assert_equal(5702, @hb.year, "Wrong year.")
+    assert_equal('התש"ב', @hb.heb_year_name, "Wrong year.")
+    assert_equal(Date.new(1941, 12, 10).jd, @hb.jd, "Wrong Julian date.")
+  end
+
+
+
+end
+
